25a3a5b7657f54f056f994eec1f67ef504d9fb9c
Mustafa Korkmaz <118779406+mkorkmaz71@users.noreply.github.com>
2023-05-11 23:41:49 -0500

    initial commit

---
+++ src/test/java/com/automation/steps/StepDefinition.java
@@ -0,0 +1,28 @@
+package com.automation.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinition {
+    @Given("user opens the website")
+    public void user_opens_the_website() {
+
+
+    }
+    @Given("verify user is on login page")
+    public void verify_user_is_on_login_page() {
+
+
+    }
+    @When("user enters valid credentials")
+    public void user_enters_valid_credentials() {
+
+
+    }
+    @Then("verify user is on home page")
+    public void verify_user_is_on_home_page() {
+
+
+    }
+}
---
+++ target/test-classes/features/Login.feature
@@ -0,0 +1,7 @@
+Feature: Verify Login Feature
+
+  Scenario: Verify user can login with valid credentials
+    Given user opens the website
+    And verify user is on login page
+    When user enters valid credentials
+    Then verify user is on home page
